881dd20f5fb8598389e1512c4004f2b68f462bb2
weiyan <weiyan@creditx.com>
2017-06-08 16:08:41 +0800

    adding https support for HTTP/2

--- lib/trot/supervisor.ex
+++ lib/trot/supervisor.ex
@@ -10,6 +10,20 @@ defmodule Trot.Supervisor do
   def init([]) do
     import Supervisor.Spec
 
+    priv_dir = Path.join(Application.app_dir(:trot), "priv")
+    certfile = Path.join(priv_dir, "server.pem")
+    keyfile = Path.join(priv_dir, "server.key")
+
+    unless File.exists?(keyfile) do
+      raise """
+      No SSL key/cert found. Please run the following command:
+
+      openssl req -new -newkey rsa:4096 -days 365 -nodes -x509  \
+      -subj "/C=CN/ST=Shanghai/L=Changning/O=CreditX/CN=creditx.com" \
+      -keyout priv/server.key -out priv/server.pem
+      """
+    end
+
     port = case Application.get_env(:trot, :port, 4000) do
       port when is_binary(port) -> String.to_integer(port)
       port -> port
@@ -17,7 +31,9 @@ defmodule Trot.Supervisor do
     router_module = Application.get_env(:trot, :router, Trot.NotFound)
 
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, router_module, [], [port: port]),
+      Plug.Adapters.Cowboy2.child_spec(:https, router_module, [],
+        [port: port, certfile: certfile, keyfile: keyfile]),
+      Plug.Adapters.Cowboy2.child_spec(:http, router_module, [], [port: port+1])
     ]
     supervise(children, strategy: :one_for_one)
   end
